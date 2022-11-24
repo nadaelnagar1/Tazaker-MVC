92370835a72bb27f620912afbbe2c712541a4059
nadaelnagar1 <nadaelnagar483@gmail.com>
2022-11-24 18:29:50 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tazaker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
